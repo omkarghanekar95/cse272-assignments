1be6f89c2d89fb7651abf987ab0074277a72640b
Omkar <omkar@Omkars-MacBook-Pro.local>
2022-05-02 23:56:30 -0700

    minor bug

--- hw1/queries.py
+++ hw1/queries.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 import logging, json, time
 from constants import file_identifiers, query_constants as qc, doc_constants as dc
-from elastic_functions import connect_elasticsearch, basic_query, bool_query, get_rank_eval_query
+from elastic_functions import connect_elasticsearch, basic_query, bool_query, get_rank_eval_query, get_fuzzy_query
 
 
 with open(qc.rels_file) as f:
@@ -34,18 +34,19 @@ def get_single_query(lines,curr, file_length):
 			curr+=1
 	return single_query, curr+1
 
-def write_results_to_file(algorithm, query, results):
+def write_results_to_file(algorithm, query_id, results, output_file):
  	#	R101 Q0 83653 1 0.430352 tfidf-Both 
 
-	if algorithm == qc.tf_idf:
- 		output_file = qc.tfidf_log_file
-
-	elif algorithm == qc.bquery:
-		output_file = qc.bool_log_file
-	# print(output_file)
 	with open(output_file, 'a+') as f:
 		for i in range(len(results)):
-			file_string = query + " Q0 "+ results[i]["_id"] +" "+ str(i+1)+" "+str(results[i]["_score"])+" "+ algorithm +"\n"
+			if algorithm == qc.rank_eval:
+				doc_id = results[i]["hit"]["_id"]
+				score = str(results[i]["hit"]["_score"])
+			else:
+				doc_id = results[i]["_id"]
+				score = str(results[i]["_score"])
+			
+			file_string = f"{query_id} Q0 {doc_id} {str(i+1)} {score} {algorithm}\n"
 			f.write(file_string)
 	logging.info("Writing results to file")
 
@@ -53,7 +54,6 @@ def parse_rels_file(qid):
 	ratings= []
 	ids = []
 	for line in relslines:
-		#print(line[:5], type(line[:5]), type(qid), qid)
 		term = line.strip('\n').split('\t')
 		if term[0] == qid:
 			ids.append(term[2])
@@ -67,22 +67,12 @@ def rank_search (es, query):
 	
 	ratings,ids = parse_rels_file(query["num"])
 	req, metric = get_rank_eval_query(query["num"], query[qc.title], ratings[:20], ids[:20])
-	alg = qc.rank_eval
-	#print(req, metric)
-	result  = es.rank_eval(index=dc.index_name, requests=req, metric =metric)
-	#print(result)
-	hits = result["details"][query["num"]]["hits"]
-	# print(len(hits),' found hits')
-	# for i in range(len(hits)):
-	# 	print(hits[i]["hit"]["_id"]," and score is ", hits[i]["hit"]["_score"])
 	
-	output_file = qc.rank_log_file
-	algorithm = "relevance_feedback"
-	with open(output_file, 'a+') as f:
-		for i in range(len(hits)):
-			file_string = query["num"] + " Q0 "+ hits[i]["hit"]["_id"] +" "+ str(i+1)+" "+str(hits[i]["hit"]["_score"])+" "+ algorithm +"\n"
-			f.write(file_string)
-	logging.info("Writing results to file")
+	result  = es.rank_eval(index=dc.index_name, requests=req, metric =metric)
+	
+	hits = result["details"][query["num"]]["hits"]
+	
+	write_results_to_file(qc.rank_eval, query["num"], hits, qc.rank_log_file)
 
 
 def search_query(es, query, algorithm):
@@ -90,17 +80,20 @@ def search_query(es, query, algorithm):
 	if algorithm == qc.tf_idf:
 		qstring= query[qc.title]+" "+query[qc.description]
 		body_query = basic_query(qstring)
-		alg = qc.tf_idf
+		output_file = qc.tfidf_log_file
+
 	elif algorithm == qc.bquery:
 		body_query = bool_query(query[qc.title], query[qc.description])
-		alg = qc.bquery
+		output_file = qc.bool_log_file
+
+	elif algorithm == qc.fuzzy:
+		qstring= query[qc.title]+" "+query[qc.description]
+		body_query = get_fuzzy_query(qstring)
+		output_file = qc.fuzzy_log_file
 	
-	#print(body_query)
 	try:
 		result = es.search(index=dc.index_name, body=body_query, track_scores= True)
-		# for res in result["hits"]["hits"]:
-		# 	print("doc is ",res["_source"]["MedID"]," and score is ",res["_score"])
-		write_results_to_file(alg, query["num"], result["hits"]["hits"])
+		write_results_to_file(algorithm, query["num"], result["hits"]["hits"], output_file)
 
 	except Exception as ex:
 		print('Error in querying data', str(ex))
@@ -119,10 +112,11 @@ if __name__ == "__main__":
 			query, curr = get_single_query(lines, curr, file_length)
 			print('searching query with id', query["num"])
 			
-			#search_query(es, query, qc.tf_idf)
-			#search_query(es, query, qc.bquery)
-			rank_search(es,query)
-			#curr+=1
-			#a+=1
+			#search_query(es, query, qc.fuzzy)
+			search_query(es, query, qc.tf_idf)
+			search_query(es, query, qc.bquery)
+			#rank_search(es,query)
+			curr+=1
+			
 		
 	print('execution time is ', time.time()-start_time)
